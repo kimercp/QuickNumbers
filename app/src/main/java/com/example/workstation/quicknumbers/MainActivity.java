f1fe06058a1591364aaa2752572649402630b931
WyrwiGnatus <30984606+kimercp@users.noreply.github.com>
2017-10-22 21:03:16 +0100

    first commit

---
+++ app/src/main/java/com/example/workstation/quicknumbers/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.workstation.quicknumbers;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MakeFullscreen();
+    }
+
+    /*   Hide UI action bar and make the app fullscreen */
+    private void MakeFullscreen() {
+        getSupportActionBar().hide();
+        // API 19 (Kit Kat)
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        } else {
+            if (Build.VERSION.SDK_INT > 10) {
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
+        }
+    }
+
+    // test github
+    // durga linia
+
+}
