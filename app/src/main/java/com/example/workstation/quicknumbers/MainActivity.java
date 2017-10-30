8847b1234c34587fe8c602ac1d15390a2711c552
WyrwiGnatus <30984606+kimercp@users.noreply.github.com>
2017-10-30 02:56:39 +0000

    Added new activitiy
    Splash screen
    Menu

--- app/src/main/java/com/example/workstation/quicknumbers/MainActivity.java
+++ app/src/main/java/com/example/workstation/quicknumbers/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.workstation.quicknumbers;
 
+import android.content.DialogInterface;
 import android.os.Build;
-import android.os.CountDownTimer;
-import android.provider.CalendarContract;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtTimer;
     private TextView txtPoints;
     private TextView txtQuestion;
+    private TextView txtAnswer;
     private int firstNumber;
     private int secondNumber;
-    private char operator = '+';
+    private int level = 1;
+    private int timeToCountdown = 10000;
+    private int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +29,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtTimer = (TextView) findViewById(R.id.txtTimer);
-        txtPoints = (TextView) findViewById(R.id.txtTimer);
+        txtPoints = (TextView) findViewById(R.id.txtPoints);
         txtQuestion = (TextView) findViewById(R.id.txtQuestion);
+        txtAnswer = (TextView) findViewById(R.id.txtAnswer);
+
+        /* Buttons */
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);
+        Button buttonclear = (Button) findViewById(R.id.buttonclear);
+        Button buttonequal = (Button) findViewById(R.id.buttonequal);
+        /* Set OnClickListener for each button */
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonclear.setOnClickListener(this);
+        buttonequal.setOnClickListener(this);
 
         MakeFullscreen();
 
-        // Draw numbers to calculate level 1
-        DrawNumbers(1);
-        // start countdown
-        Countdown(30000);
+        // Draw numbers to calculate level 1 (max 9 levels)
+        DrawNumbers(level);
+        // start countdown (30 secs)
+        Countdown(timeToCountdown);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) findViewById(v.getId());
+        String text = btn.getText().toString();
+
+        switch (v.getId()){
+            case R.id.buttonclear:
+                ClearTextResult();
+                break;
+            case R.id.buttonequal:
+                if (!txtAnswer.getText().toString().isEmpty()) CompareResult();
+                break;
+            default:
+                String currentText = txtAnswer.getText().toString();
+                if (currentText.length() < 3) txtAnswer.setText(currentText+text);
+                break;
+
+        }
+    }
+
+    private void CompareResult() {
+        if (firstNumber+secondNumber == Integer.parseInt(txtAnswer.getText().toString())) {
+            // Change this for sound
+            Toast.makeText(this,"Bravo",Toast.LENGTH_SHORT).show();
+            points++;
+            DrawNumbers(level);
+        }
+        else Toast.makeText(this,"Wrong",Toast.LENGTH_SHORT).show(); // Change this for sound
+        // reset textview with users result
+        ClearTextResult();
+    }
+
+    private void ClearTextResult() {
+        txtAnswer.setText("");
     }
 
     /* Hide UI action bar and make the app fullscreen */
@@ -75,22 +141,44 @@ public class MainActivity extends AppCompatActivity {
                     txtTimer.setText(Integer.toString(minutesLeft) + ":0" + secondsLeft);
                 else
                     txtTimer.setText(Integer.toString(minutesLeft) + ":" + secondsLeft);
+
+                txtPoints.setText("Points: " + Integer.toString(points));
             }
 
             @Override
             public void onFinish() {
-                // call alarm on mobile device
-                txtTimer.setText("Alarm");
+                // Display the points and ask if user wants restart game
+
+                new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogStyle)
+                    .setTitle(R.string.dialog_tryAgain)
+                    .setMessage(R.string.dialog_again)
+                    .setIcon(null)
+                    .setPositiveButton(R.string.dialog_Yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+//  new activity , splash screen ???????
+                        }
+                    })
+                    .setNegativeButton(R.string.dialog_No, new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .show();
+                // reset all game variables to default values
+                ResetGameVariables();
             }
         }.start();
     }
 
-    /* Compare the results */
-    private void Compare(int result){
-//        txtQuestion.setText(Integer.toString(firstNumber) + " + " + Integer.toString(secondNumber) + " = " + Integer.toString(result));
-//        if (firstNumber+secondNumber == result) Toast.makeText(this,"Bravo",Toast.LENGTH_SHORT).show();
-
-        1. klik na cyfre, dodaj to textview . |Dodaj rownosc try {
-        wtedy po kliknieciu na rownosc urucho compare( results from textview)
+    /* Reset all game variables to default values */
+    private void ResetGameVariables(){
+        points = 0;
+        level = 1;
+        timeToCountdown = 30000;
+        firstNumber = 0;
+        secondNumber = 0;
+        ClearTextResult();
     }
 }
